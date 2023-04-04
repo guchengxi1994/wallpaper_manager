f90602871d5b8c8a4661ab7ea5c679cff6c314c3
壹零 <guchengxi1994@qq.com>
2023-04-04 10:39:58 +0800

    chore : add some tests

--- native/src/tests/mod.rs
+++ native/src/tests/mod.rs
@@ -2,3 +2,9 @@ mod test_change_wall_paper;
 mod test_get_windows_screen_size;
 mod test_windows_toast;
 mod test_get_windows_hwnd;
+#[allow(dead_code)]
+#[allow(unused_imports)]
+mod test_multi_monitor;
+#[allow(dead_code)]
+#[allow(unused_imports)]
+mod test_multi_monitor_2;
